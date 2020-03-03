1cded098fb2de78cb2539728e0dff1d2a31c9fdf
578041 <54099050+578041@users.noreply.github.com>
2020-03-03 16:09:29 +0100

    Add files via upload

---
+++ ListeOrdnetU2/src/no/hvl/dat102/tabell/TabellOrdnetListe.java
@@ -0,0 +1,169 @@
+package no.hvl.dat102.tabell;
+
+import no.hvl.dat102.EmptyCollectionException;
+import no.hvl.dat102.adt.OrdnetListeADT;
+
+public class TabellOrdnetListe<T extends Comparable<T>> implements OrdnetListeADT<T> {
+
+	private final static int STDK = 100;
+	private final static int IKKE_FUNNET = -1;
+	private int bak;
+	private T[] liste;
+
+	public TabellOrdnetListe() {
+		this(STDK);
+	}
+
+	public TabellOrdnetListe(int startKapasitet) {
+		bak = 0;
+		liste = (T[]) (new Comparable[startKapasitet]);
+	}
+
+	@Override
+	public T fjernSiste() {
+		if (erTom())
+			throw new EmptyCollectionException("ordnet liste");
+
+		T resultat = null;
+		resultat = liste[bak-1];
+		liste[bak-1] = null;
+		bak--;
+		return resultat;
+	}
+
+	@Override
+	public T fjernFoerste() {
+		if (erTom())
+			throw new EmptyCollectionException("ordnet liste");
+
+		T resultat = null;
+		 resultat = liste[0];
+		liste[0] = null;
+		for(int i = 0; i<bak-1;i++) {
+			liste[i] = liste[i+1];
+		}
+		bak--;
+		return resultat;
+	}
+
+	@Override
+	public T foerste() {
+		if (erTom())
+			throw new EmptyCollectionException("ordnet liste");
+
+		T resultat = liste[0];
+		return resultat;
+	}
+
+	@Override
+	public T siste() {
+		if (erTom())
+			throw new EmptyCollectionException("ordnet liste");
+		
+		T resultat = null;
+		resultat = liste[bak];
+
+		return resultat;
+	}
+
+	@Override
+	public boolean erTom() {
+		return (bak == 0);
+	}
+
+	@Override
+	public int antall() {
+		return bak;
+	}
+
+	
+		@Override
+		public void leggTil(T element) {
+
+			if (erTom()) {
+				liste[0] = element;
+				bak++;
+			}
+
+			else {
+
+				if (bak >= liste.length) {
+					utvid();
+				}
+
+				boolean lagtTil = false;
+				int i = 0;
+
+				while (i < bak && !lagtTil) {
+					if (liste[i].compareTo(element) >= 0) {
+						bak++;
+						for (int j = bak; j > i; j--) {
+							liste[j] = liste[j - 1];
+						}
+						liste[i] = element;
+						lagtTil = true;
+					}
+					i++;
+				}
+				if(!lagtTil) {
+					liste[bak] = element;
+					bak++;
+				}
+			}
+		}
+
+	@Override
+	public boolean inneholder(T element) {
+		return (finn(element) != IKKE_FUNNET);
+	}
+
+	@Override
+	public T fjern(T element) {
+	
+		int indeks = finn(element);
+		if (indeks == -1) {
+			return null;
+		}
+		liste[indeks] = null;
+		for( int i = indeks;i < bak-1; i++ ) {
+			liste[i] = liste[i+1];
+			
+		}
+		bak--;
+		return element;
+
+	}
+
+	private int finn(T el) {
+		int resultat = IKKE_FUNNET;
+		boolean funnet = false;
+		for(int j = 0; j < bak && !funnet ;j++) {
+		if(liste[j].equals(el)) {
+			funnet= true;
+			resultat = j;
+			
+		}
+		}
+		return resultat;
+	}
+
+	public String toString() {
+		String resultat = "";
+
+		for (int i = 0; i < bak; i++) {
+			resultat = resultat + liste[i].toString() + "\n";
+		}
+		return resultat;
+	}
+
+	private void utvid() {
+		T[] hjelpeTabell = (T[]) (new Comparable[liste.length * 2]);
+
+		for (int i = 0; i < liste.length; i++) {
+			hjelpeTabell[i] = liste[i];
+		}
+
+		liste = hjelpeTabell;
+	}
+
+}// class
